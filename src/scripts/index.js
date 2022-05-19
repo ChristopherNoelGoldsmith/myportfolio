26635affa6c1d3fe02bf90cba83b363ea4080ed3
Christopher Goldsmith <ChristopherGoldsmith7@yahoo.com>
2022-05-19 03:17:07 -0700

    UI OVERHAUL update added project desc

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,37 +1,71 @@
 //sass
-import '../sass/styles.scss';
-import '../sass/about_me.scss';
-import '../sass/my_tech_stack.scss';
-import '../sass/navbar.scss';
-import '../sass/project_showcase.scss';
-import '../sass/contact_me.scss';
+import "../sass/styles.scss";
+import "../sass/about_me.scss";
+import "../sass/my_tech_stack.scss";
+import "../sass/navbar.scss";
+import "../sass/project_showcase.scss";
+import "../sass/contact_me.scss";
+import "../sass/module.scss";
 //scripts
-import runCascadingBubbles from './bubbles';
+import runCascadingBubbles from "./bubbles";
 //images
-import '../img/oceanfloor.jpg';
-import makeSomeFlashCards from '../img/makesomeflashcards.jpg';
-import portrait from '../img/portrait.jpg';
-import weatherProject from '../img/weather_project.png';
+import "../img/oceanfloor.jpg";
+import makeSomeFlashCards from "../img/makesomeflashcards.jpg";
+import portrait from "../img/portrait.jpg";
+import weatherProject from "../img/weather_project.jpg";
 
-const toggleVis = (el) => {
+(() => {
+  const smoothScroll = (tar, dur) => {
+    const target = document.querySelector(tar);
+    const targetPosition = target.getBoundingClientRect().top;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+    const animation = (curTime) => {
+      if (startTime === null) startTime = curTime;
+      const timeElapsed = startTime - curTime;
+      const run = ease(timeElapsed, startPosition, distance, dur);
+      window.scrollTo(0, run);
+      if (timeElapsed < dur) requestAnimationFrame(animation);
+    };
+    const ease = (t, b, c, d) => {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+    requestAnimationFrame(animation);
+  };
+
+  const populateImages = () => {
+    const myPortrait = (document.getElementById("my-image").src = portrait);
+    const project1 = (document.getElementById("project1-img").src =
+      weatherProject);
+    const project2 = (document.getElementById("project2-img").src =
+      makeSomeFlashCards);
+  };
+
+  const toggleVis = (el) => {
     const elementToMakeVis = document.querySelector(el).classList;
-    if (!elementToMakeVis.contains('vis')) return elementToMakeVis.add('vis');
-    return elementToMakeVis.remove('vis');
-};
+    if (!elementToMakeVis.contains("vis")) return elementToMakeVis.add("vis");
+    return elementToMakeVis.remove("vis");
+  };
 
-document.getElementById('vis-btn').addEventListener('click', () => {
-    toggleVis('#menu');
-})
+  document.querySelector(".module").addEventListener("click", () => {
+    return toggleVis(".module");
+  });
 
-document.getElementById("contact-me-form").addEventListener('submit', (event) => {
-    console.log(document.forms.contact.every());
-})
+  document.querySelector("#contact-nav").addEventListener("click", () => {
+    return smoothScroll("#contact-me", 1000);
+  });
 
-const populateImages = () => {
-    const myPortrait = document.getElementById('my-image').src = portrait;
-    const project1 = document.getElementById('project1').src = weatherProject;
-    const project2 = document.getElementById('project2').src = makeSomeFlashCards;
-}
-
-runCascadingBubbles();
-populateImages();
+  document
+    .getElementById("contact-me-form")
+    .addEventListener("submit", (event) => {
+      const module = ".module";
+      document.forms["contact-me-form"].reset();
+      toggleVis(module);
+    });
+  runCascadingBubbles();
+  populateImages();
+})();
