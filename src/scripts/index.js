13d920962975c340e54be2d91164cd0516ee3ce3
ChristopherNoelGoldsmith <ChristopherGoldsmith101@yahoo.com>
2022-05-23 09:37:16 -0700

    scroll feature fixed, some changes to UI

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,23 +18,10 @@ import weatherProject from "../img/weather_project.jpg";
   const smoothScroll = (tar, dur) => {
     const target = document.querySelector(tar);
     const targetPosition = target.getBoundingClientRect().top;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    let startTime = null;
-    const animation = (curTime) => {
-      if (startTime === null) startTime = curTime;
-      const timeElapsed = startTime - curTime;
-      const run = ease(timeElapsed, startPosition, distance, dur);
-      window.scrollTo(0, run);
-      if (timeElapsed < dur) requestAnimationFrame(animation);
-    };
-    const ease = (t, b, c, d) => {
-      t /= d / 2;
-      if (t < 1) return (c / 2) * t * t + b;
-      t--;
-      return (-c / 2) * (t * (t - 2) - 1) + b;
-    };
-    requestAnimationFrame(animation);
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth'
+    });
   };
 
   const populateImages = () => {
